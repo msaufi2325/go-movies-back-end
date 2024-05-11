62cb4a2ebb119d4a1d4bf0d67ed8e90f03315979
Saufi <msaufi2325@gmail.com>
2024-05-12 05:24:09 +0900

    Add basic web server functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
 func main() {
 	// set application config
+	var app application
+
+	// read from command line
+
+	// connect to database
+
+	app.Domain = "example.com"
+
+	// start a web server
+	fmt.Printf("Starting application on port %d\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
