f97109d74b66e9b3dae97347b568c140ebc002ce
Saufi <msaufi2325@gmail.com>
2024-05-12 17:09:57 +0900

    Add route handler for /movies path

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -26,3 +26,7 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(out)
 }
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
