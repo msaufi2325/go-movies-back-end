3cd4b00769d51d2b51c47cdf7196add8f89813da
msaufi2325 <msaufi2325@gmail.com>
2024-06-10 21:50:00 +0900

    Add API key configuration option to main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -22,6 +22,7 @@ type application struct {
 	JWTIssuer    string
 	JWTAudience  string
 	CookieDomain string
+	APIKey       string
 }
 
 func main() {
@@ -35,6 +36,7 @@ func main() {
 	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
 	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
 	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "de849523ca83fd117ee9d058528ecdbd", "api key")
 	flag.Parse()
 
 	// connect to database
